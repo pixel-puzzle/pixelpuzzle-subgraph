e2d41828e895d898830c40027d7f024320e552bc
HelmetPink <ji2240224966@163.com>
2021-09-15 12:09:47 +0800

    'test-graph'

--- src/OptionNft.ts
+++ src/OptionNft.ts
@@ -1,6 +1,7 @@
 import { Transfer } from '../generated/OptionNft/OptionNft'
+import { Option } from '../generated/schema'
 export function handleTransfer(event: Transfer): void {
-    let option = new Transfer(event.params.tokenId.toHex())
+    let option = new Option(event.params.tokenId.toHex())
     option.from = event.params.from
     option.to = event.params.to
     option.tokenId = event.params.tokenId
